78d640a139fadbfa41b6850abd0b079c2bf35fe0
Omer Eren <neumann@Omers-MacBook-Pro.local>
2017-04-21 15:07:19 +0300

    Min maj mappings are added

--- labreader.py
+++ labreader.py
@@ -101,6 +101,64 @@ def gen_beatles_dataset():
 
     generate_dataset(imap, inp_folder, out_folder)
 
+chord_map = {}
+cnt = 1
+for qual in ['maj', 'min']:
+    for root in ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']:
+        chord_map['{}:{}'.format(root,qual)] = cnt
+        cnt += 1
+def chord_to_category(chord):
+    return chord_map[chord]
+
+def is_subset(bitmap, ground):
+    for i in range(len(bitmap)):
+        if bitmap[i] == 0 and ground[i] == 1:
+            return False
+    return True 
+
+QUALITIES = {
+    #           1     2     3     4  5     6     7
+    'maj':     [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    'min':     [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    'aug':     [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    'dim':     [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    'sus4':    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    'sus2':    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    '7':       [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    'maj7':    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    'min7':    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    'minmaj7': [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+    'maj6':    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+    'min6':    [1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
+    'dim7':    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    'hdim7':   [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+    'maj9':    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    'min9':    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    '9':       [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    'b9':      [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    '#9':      [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    'min11':   [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    '11':      [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    '#11':     [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    'maj13':   [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    'min13':   [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    '13':      [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    'b13':     [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    '1':       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    '5':       [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    '':        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+
+def get_majminchord(chord):
+    root, quality, bass = mir_eval.chord.encode(chord)
+    root_char = mir_eval.chord.split(chord)[0]
+    if root_char == 'N':
+        return root_char
+    if is_subset(quality, QUALITIES['maj']):
+       return "{}:{}".format(root_char, 'maj')
+    if is_subset(quality, QUALITIES['min']):
+       return "{}:{}".format(root_char, 'min')
+    return 'U'
+
 def calc_chord_freqs():
     data = read_folder_folder('/Users/neumann/bilkent/eee485/project/chordemall/data/beatles/chordlab/The Beatles/')
     pp = pprint.PrettyPrinter(indent=3)
@@ -114,7 +172,8 @@ def calc_chord_freqs():
             else:
                 alph[chord[2]] += 1;
     print("ALPHABETSIZE: {}".format(len(alph)))
-    pp.pprint([(key, mir_eval.chord.encode(key), alph[key]) for key in alph])
+    pp.pprint([(key, get_majminchord(key), alph[key]) for key in alph])
 
 if __name__ == '__main__':
-    gen_beatles_dataset()
+    calc_chord_freqs()
+    #gen_beatles_dataset()
