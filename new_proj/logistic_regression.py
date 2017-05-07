d9c5e3e0356a96287a27a33eef35a0b576b3af60
Omer Eren <omerrn@gmail.com>
2017-05-07 21:41:09 +0300

    logigigig

--- new_proj/logistic_regression.py
+++ new_proj/logistic_regression.py
@@ -19,13 +19,23 @@ class LogisticRegression(object):
         for i in range(num_epoch):
             err = 0
             for j in range(X.shape[0]):
-                y_cap = X[i].dot(self.W)
-                err = 
+                delta = []
+                batch_x = (batch_x - self.mean)/self.std;
+                X_new.append(np.concatenate((np.ones((batch_x.shape[0],1)),
+                                              batch_x), axis=1))
+                new_x = np.concatenate((np.ones((V_batch[l].shape[0],1)), new_x),
+                                        axis=1)
+
+                self.W = self.W - (1./self.batch_size)*self.learning_rate*delta
 
     def predict(self, X):
-
-
-
+        delta = []
+        batch_x = (batch_x - self.mean)/self.std;
+        X_new.append(np.concatenate((np.ones((batch_x.shape[0],1)),
+                                    batch_x), axis=1))
+        new_y = np.concatenate((np.ones((V_batch[l].shape[0],1)), new_x),
+                                        axis=1)
+        return new_y
 
 def train_logistic():
     X = pickle.load(open('../data/interim/dnn_chroma.p', 'rb'))
@@ -49,14 +59,47 @@ def train_logistic():
     for j in range(n):
         new_y.append(chord.chord_to_category(chord.get_majminchord(y[num_data][1][j])))
 
-    for i in range(1, num_test):
-        n = min(X[i+num_data].shape[0], len(y[i+num_data][1])-7)
-
-        new_X = np.concatenate((new_X, X[i+num_data][:n]), axis=0)
+    true_pred = []
+    all_pred = []
+    num_categories = 25
+    conf = np.zeros((num_categories, num_categories), dtype=np.float64)
+    #for i in range(len(X)):
+    for i in range(5):
+        n = min(X[i].shape[0], len(y[i][1])-7)
+        new_y = []
         for j in range(n):
-            new_y.append(chord.chord_to_category(chord.get_majminchord(y[i+num_data][1][j])))
- 
-    print(model.predict(new_X, new_y))
+            new_y.append(chord.chord_to_category(chord.get_majminchord(y[i][1][j])))
+        y_cap = model.predict(X[i][:n])
+        err = mir_eval.chord.mirex([chord.category_to_chord(new_y[j]) for j in range(len(new_y))],
+                                   [chord.category_to_chord(y_cap[j]) for j in range(len(y_cap))])
+        true_pred.append(np.sum(err))
+        all_pred.append(len(err))
+        for j in range(n):
+            conf[new_y[j]-1][y_cap[j]-1] += 1
+        
+        print("Song#{}".format(i))
+
+    acc = float(sum(true_pred))/sum(all_pred)
+    std = math.sqrt((1./sum(all_pred))*sum([(true_pred[i] - acc)**2 for i in range(len(true_pred))]))
+    print("ACCURACY: {} - STD: {}".format(acc, std))
+
+    for i in range(len(conf)):
+        if sum(conf[i]) != 0:
+            conf[i] = [float(conf[i][j]) / sum(conf[i]) for j in range(len(conf[i]))]
+
+    fig, ax = plt.subplots()
+    heatmap = ax.pcolor(conf, cmap=plt.cm.Blues, alpha=0.8)
+    ax.set_yticks(np.arange(len(conf))+0.5, minor=False)
+    ax.set_xticks(np.arange(len(conf[0]))+0.5, minor=False)
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    ax.set_xticklabels([chord.category_to_chord(i) for i in range(1, 26)])
+    ax.set_yticklabels([chord.category_to_chord(i) for i in range(1, 26)])
+    plt.xticks(rotation=90)
+
+    plt.show()
 
 if __name__ == '__main__':
     train_logistic()
